7c5d00102c33759dc408802198d9c22299353f77
Hunter Freyer <yt@hjfreyer.com>
2017-12-15 20:41:20 -0500

    Bring polymer 2 version up to date.

---
+++ src/ww-app/game.js
@@ -0,0 +1,230 @@
+import * as _ from '../../node_modules/lodash-es/lodash.js';
+
+const ROUND_LENGTH_SEC = 180;
+
+const STATE_WAITING_FOR_NEXT_GAME = 'WAITING_FOR_NEXT_GAME';
+const STATE_WAITING_FOR_NEXT_ROUND = 'WAITING_FOR_NEXT_ROUND';
+const STATE_ROUND = 'ROUND';
+
+export class Controller {
+  constructor(wordList) {
+    this._wordList = wordList;
+
+    this._state = STATE_WAITING_FOR_NEXT_GAME;
+  }
+
+  newGame() {
+    this._checkState(STATE_WAITING_FOR_NEXT_GAME) ;
+
+    this._state = STATE_WAITING_FOR_NEXT_ROUND;
+    this._score = 0;
+    return {};
+  }
+
+  newRound() {
+    this._checkState(STATE_WAITING_FOR_NEXT_ROUND) ;
+
+    let gameStructure = _.sample(this._wordList.containment);
+    let tiles = _.shuffle(
+      this._wordList.words[gameStructure.fullWord].split(''));
+    let answers = gameStructure.subwords.map(i => this._wordList.words[i]);
+    answers = _.sortBy(answers, ['length', _.identity]);
+
+    this._tiles = tiles.map((t, idx) => ({
+      letter: t,
+      slotIdx: idx,
+      isSuggestion: false,
+    }));
+    this._answers = answers.map(a => ({
+      answer: a,
+      guessed: false,
+    }));
+    this._available = [...tiles.keys()]; // Range
+    this._suggestions = [];
+    this._endTime = Date.now() + ROUND_LENGTH_SEC * 1000;
+
+    this._state = STATE_ROUND;
+
+    return {
+      tiles: tiles,
+      answers: answers,
+      endTime: this._endTime,
+    };
+  }
+
+  selectTile(tileIdx) {
+    this._checkState(STATE_ROUND);
+
+    let tile = this._tiles[tileIdx];
+    if (tile.isSuggestion) {
+      if (tile.slotIdx != this._suggestions.length - 1) {
+        return {
+          moves: []
+        };
+      }
+      return this.backspace();
+    } else {
+      this._suggestions.push(tileIdx);
+      this._available[tile.slotIdx] = -1;
+      tile.slotIdx = this._suggestions.length - 1;
+      tile.isSuggestion = true;
+      return {
+        moves: [{
+          tileIdx: tileIdx,
+          position: {
+            slot: tile.slotIdx,
+            isSuggestion: tile.isSuggestion,
+
+          }
+        }]
+      };
+    }
+    return {
+      moves: []
+    };
+  }
+
+  heartbeat() {
+    if (this._state != STATE_ROUND) {
+      return {};
+    }
+    if (this._endTime < Date.now()) {
+      this._state = STATE_WAITING_FOR_NEXT_ROUND;
+      let toReveal = _.filter([...this._answers.keys()],
+        idx => !this._answers[idx].guessed);
+
+      return {
+        roundEnd: {
+          reveal: toReveal,
+        },
+      };
+    }
+    return {};
+  }
+
+  backspace() {
+    this._checkState(STATE_ROUND);
+    if (this._suggestions.length == 0) {
+      return {
+        moves: []
+      };
+    }
+
+    return {
+      moves: [this._doBackspace()]
+    };
+  }
+
+  _doBackspace() {
+    let tileIdx = this._suggestions[this._suggestions.length - 1];
+    let tile = this._tiles[tileIdx];
+
+    let dest = this._nthEmpty(tile.slotIdx);
+    this._available[dest] = tileIdx;
+    this._suggestions.pop();
+    tile.slotIdx = dest;
+    tile.isSuggestion = false;
+    return {
+      tileIdx: tileIdx,
+      position: {
+        slot: tile.slotIdx,
+        isSuggestion: tile.isSuggestion,
+      }
+    };
+  }
+
+  _nthEmpty(n) {
+    let res = -1;
+    for (let i = 0; i < n + 1; i++) {
+      res = _.findIndex(this._available, s => s == -1, res + 1);
+    }
+    return res;
+  }
+
+
+  submit() {
+    this._checkState(STATE_ROUND);
+
+    let guess = this._suggestions.map(tileIdx => this._tiles[tileIdx].letter).join('');
+    let answerIdx = _.findIndex(this._answers, {
+      'answer': guess
+    });
+    if (answerIdx != -1) {
+      if (this._answers[answerIdx].guessed) {
+        return {
+          duplicate: {
+            answerIdx: answerIdx
+          },
+          score: this._score,
+          moves: _.times(guess.length, this._doBackspace.bind(this)),
+        }
+      } else {
+        this._answers[answerIdx].guessed = true;
+        this._score += guess.length * guess.length;
+        return {
+          accept: {
+            answerIdx: answerIdx,
+          },
+          score: this._score,
+          moves: _.times(guess.length, this._doBackspace.bind(this)),
+        };
+      }
+    } else {
+      return {
+        reject: {},
+        score: this._score,
+        moves: [],
+      };
+    }
+  }
+
+
+  // TODO: This could be a bit prettier by only swapping filled slots, not
+  // moving things to entirely new slots.
+  shuffle() {
+    this._checkState(STATE_ROUND);
+
+    let moves = [];
+
+    let perm = _.shuffle(_.range(6));
+    let newAvailable = [];
+    for (let i = 0; i < perm.length; i++) {
+      newAvailable[perm[i]] = this._available[i];
+      if (this._available[i] != -1) {
+        this._tiles[this._available[i]].slotIdx = perm[i];
+        moves.push({
+          tileIdx: this._available[i],
+          position: {
+            slot: perm[i],
+            isSuggestion: false,
+          }
+        });
+      }
+    }
+
+    this._available = newAvailable;
+    return {
+      moves: moves
+    };
+  }
+
+  typeCharacter(char) {
+    this._checkState(STATE_ROUND);
+
+    // TODO: Select the leftmost matching letter.
+    let index = _.findIndex(this._tiles, (t) => t.letter == char && !t.isSuggestion);
+    if (index == -1) {
+      return {
+        moves: []
+      };
+    }
+
+    return this.selectTile(index);
+  }
+
+  _checkState(expectedState) {
+    if (this._state != expectedState) {
+      throw 'Invalid game state';
+    }
+  }
+}
---
+++ src/ww-app/view.js
@@ -0,0 +1,201 @@
+import { Controller } from './game.js';
+import { WORDS } from './words.js';
+import * as _ from '../../node_modules/lodash-es/lodash.js'
+import * as util from './util.js'
+import * as layout from './layout.js'
+
+class App extends Polymer.Element {
+ static get is() {
+   return 'ww-app';
+ }
+ static get properties() {
+   return {
+     prop1: {
+       type: String,
+       value: 'ww-app'
+     }
+   };
+ }
+
+  constructor() {
+    super();
+    this._c = new Controller(WORDS);
+    this._newGame();
+    this._newRound()
+
+    this.wrong = false;
+    this.showRoundOver = false;
+    this.showGameOver = false;
+
+    document.addEventListener('keydown', this._handleKeydown.bind(this));
+    setInterval(this._heartbeat.bind(this), 1000);
+  }
+
+  _newGame() {
+    this._c.newGame();
+    this.score = 0;
+    this.showGameOver = false;
+  }
+
+  _newRound() {
+    this.showRoundOver = false;
+
+    let resp = this._c.newRound();
+    console.log(this._c);
+
+    this.tiles = resp.tiles.map((t, idx) => ({
+      letter: t,
+      idx: idx,
+      position: {
+        slot: idx,
+        isSuggestion: false
+      },
+    }));
+    this.answers = resp.answers.map(a => ({
+      word: a,
+      hidden: true,
+      spaces: _.times(a.length, () => '_ ').join('')
+    }));
+    this._endTime = resp.endTime;
+
+    this._resetButtons();
+  }
+
+  lettersOf(word) {
+    return word.split('');
+  }
+
+  styleOf(wordIdx, letterIdx) {
+    let lh = new layout.LayoutHelper(this._layout);
+
+    let pos = lh.getLetterCoordinates(wordIdx, letterIdx);
+    return `
+      left: ${pos.x}px;
+      top: ${pos.y}px;
+      width: ${this._layout.tileSizePx}px;
+      height: ${this._layout.tileSizePx}px;
+      font-size: ${this._layout.tileSizePx * 0.63}px;
+    `;
+  }
+
+  nextRound() {
+    this._newRound();
+  }
+  ready() {
+    super.ready();
+
+    let answers = _.map(this.answers, 'word');
+
+    this._layout = layout.getOptimalLayout(answers, 0.2, 1.2, this.$.answers.offsetWidth, this.$.answers.offsetHeight);
+
+    answers.forEach((a, wordIdx) => {
+      for (let letterIdx = 0; letterIdx < a.length; letterIdx++) {
+        let box = document.createElement('div');
+        box.className = 'box';
+        this.$.answers.appendChild(box);
+      }
+    });
+  }
+
+  _updateTime() {
+    let timeLeft = Math.max(0, this._endTime - Date.now());
+    this.time = util.formatMillis(timeLeft);
+  }
+
+  _heartbeat() {
+    this._updateTime();
+    let resp = this._c.heartbeat();
+    if (resp.roundEnd) {
+      this.showRoundOver = true;
+      for (let answerIdx of resp.roundEnd.reveal) {
+        this._reveal(answerIdx);
+      }
+    }
+  }
+
+  _handleKeydown(e) {
+    let charCode = e.keyCode;
+    const [a, z, A, Z] = ['a', 'z', 'A', 'Z'].map((s) => s.charCodeAt(0));
+    if (a <= charCode && charCode <= z) {
+      this._typeChar(String.fromCharCode(charCode));
+    } else if (A <= charCode && charCode <= Z) {
+      this._typeChar(String.fromCharCode(charCode - A + a));
+    } else if (e.code == 'Backspace') {
+      e.preventDefault();
+      this._backspace();
+    } else if (e.code == 'Enter') {
+      e.preventDefault();
+      this._submit();
+    } else if (e.code == 'Space') {
+      e.preventDefault();
+      this._shuffle();
+    }
+    return false;
+  }
+
+  _typeChar(c) {
+    this._moveTiles(this._c.typeCharacter(c).moves);
+  }
+
+  select(selectEvent) {
+    if (selectEvent.model.item == -1) {
+      return;
+    }
+    this._moveTiles(this._c.selectTile(selectEvent.model.item.idx).moves);
+  }
+
+  _moveTiles(moves) {
+    moves.forEach(m => this._moveTile(m.tileIdx, m.position));
+  }
+
+  _moveTile(tileIdx, pos) {
+    this.set(['tiles', tileIdx, 'position'], pos);
+    this._resetButtons();
+  }
+
+  _resetButtons() {
+    let suggestions = this.tiles.map(() => ({
+      letter: '',
+      idx: -1
+    }));
+    let available = this.tiles.map(() => ({
+      letter: '',
+      idx: -1
+    }));
+    this.tiles.map((t, idx) => {
+      (t.position.isSuggestion ? suggestions : available)[t.position.slot] = {
+        letter: t.letter,
+        idx: idx,
+      };
+    });
+
+    this.suggestions = suggestions;
+    this.available = available;
+  }
+
+  _reveal(answerIdx) {
+    this.set(['answers', answerIdx, 'hidden'], false);
+  }
+
+  _submit() {
+    let resp = this._c.submit();
+    this.score = resp.score;
+    this._moveTiles(resp.moves);
+    if (resp.accept) {
+      this._reveal(resp.accept.answerIdx);
+    }
+    if (resp.reject) {
+      this.wrong = true;
+      window.setTimeout(() => this.wrong = false, 1000);
+    }
+  }
+
+  _shuffle() {
+    this._moveTiles(this._c.shuffle().moves);
+  }
+
+  _backspace() {
+    this._moveTiles(this._c.backspace().moves);
+  }
+}
+customElements.define('ww-app', App);
