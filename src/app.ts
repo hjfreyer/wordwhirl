dd445bd2e8b62fa99764a994970a4c03e49eeeaf
Hunter Freyer <yt@hjfreyer.com>
2018-01-16 17:47:31 -0500

    Proof of concept with polymer 3.

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
 
+import '../node_modules/@webcomponents/webcomponentsjs/webcomponents-loader.js';
 import {x} from './dep.ts';
+import './poly.ts';
 
 function component() {
     var element = document.createElement('div');
